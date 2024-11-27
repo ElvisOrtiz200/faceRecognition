27cfce68161f6fdfc9ea37ee3508fe0e589cf652
ElvisOrtiz200 <170133104+ElvisOrtiz200@users.noreply.github.com>
2024-11-27 13:44:33 -0500

    weqwe

--- main.py
+++ main.py
@@ -15,15 +15,16 @@ DB_CONFIG = {
     'port': 58330,
     'user': 'root',
     'password': 'rqSlIlxswhTrVEEdgwzoLIXFAjWGFkbx',
-    'database': 'pruebaApi'
+    'database': 'pruebaApi',
+    'cursorclass': pymysql.cursors.DictCursor,  # Esto asegura que los resultados sean diccionarios
+    'connect_timeout': 10  # Establece un tiempo de espera de conexión para evitar bloqueos largos
 }
-
 
 @app.route('/asistencias', methods=['GET'])
 def get_asistencias():
     try:
         connection = pymysql.connect(**DB_CONFIG)
-        cursor = connection.cursor(pymysql.cursors.DictCursor)
+        cursor = connection.cursor()
         query = "SELECT * FROM asistenciaPersonal"
         cursor.execute(query)
         asistencias = cursor.fetchall()
@@ -80,6 +81,44 @@ def recognize():
     
     return jsonify({'faces': results})
     # Retornar los resultados como JSON
+
+
+
+@app.route('/api/asistencia', methods=['POST'])
+def insertar_asistencia():
+  
+        # Obtener los datos del cuerpo de la solicitud
+        data = request.json
+        fecha = data.get('fecha')
+        estado = data.get('estado')  # Entrada o Salida
+        personal = data.get('personal')
+        descripcion = data.get('descripcion')  # Descripción adicional (Entrada/Salida)
+
+        # Validación de que los datos necesarios están presentes
+        if not all([ fecha, estado, personal, descripcion]):
+            return jsonify({'error': 'Faltan datos en la solicitud'}), 400
+
+        # Conexión a la base de datos
+        connection = pymysql.connect(**DB_CONFIG)
+        cursor = connection.cursor()
+
+        # Insertar en la tabla asistenciaPersonal
+        query = """
+        INSERT INTO asistenciaPersonal ( fecha, estado, personal, descripcion)
+        VALUES ( %s, %s, %s, %s)
+        """
+        cursor.execute(query, (fecha, estado, personal, descripcion))
+        connection.commit()
+
+        return jsonify({'message': 'Asistencia registrada correctamente'}), 200
+
+
+
+
+    
+
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
